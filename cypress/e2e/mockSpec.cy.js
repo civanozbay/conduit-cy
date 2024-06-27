a6476867ad5fb4e2aea25e21243ebdcc0ef8fc86
civanozbay <civan417@hotmail.com>
2024-06-27 17:40:14 +0200

    mock test added

---
+++ cypress/e2e/mockSpec.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="Cypress" />
+import landingPage from "./pages/LandingPage";
+describe("Mock Testing", function () {
+  beforeEach(function () {
+    cy.visit("/");
+    cy.fixture("conduitLoginData").then((data) => {
+      this.data = data;
+      landingPage
+        .clickSigninButton()
+        .enterEmail(this.data.validEmail)
+        .enterPasssword(this.data.validPassword)
+        .clickSigninButton();
+    });
+  });
+
+  it("Tag Mock", function () {
+    cy.intercept("GET", "/api/tags", { fixture: "Tags.json" });
+    cy.contains("Your Feed").should("be.visible");
+    cy.get(".tag-list").should("contain", "reiciendis");
+  });
+
+  it("Article Mock", function () {
+    cy.intercept("GET", "/api/articles/*", { fixture: "Article.json" });
+    cy.contains("Your Feed").should("be.visible");
+    cy.contains("Global Feed").click();
+    cy.get(".col-md-9").should("contain", "Demo Mock Cypress");
+  });
+
+  it("Login User Mock", function () {
+    cy.intercept("POST", "/api/users/login", { fixture: "Login.json" });
+    cy.get('a[href^="#@"]').should("contain", "Civan");
+  });
+});
